621a91529f5635646d5cb3bca570c595fe750f54
PrasaathIBE <prasaathibe@gmail.com>
2025-08-08 22:06:05 +0530

    first commit

---
+++ 1_Introduction/react_agent_basic.py
@@ -0,0 +1,28 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain.agents import initialize_agent, tool
+from langchain_community.tools import TavilySearchResults
+import datetime
+
+load_dotenv()
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.0)
+
+search_tool = TavilySearchResults(search_depth="basic")
+
+
+@tool
+def get_system_time(format = "%Y-%m-%d %H:%M:%S") -> str:
+    """Get the current system time formatted as a string."""
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime(format)
+    return formatted_time
+    
+tools = [search_tool, get_system_time]
+agent = initialize_agent(tools=tools, llm=llm, agent="zero-shot-react-description", verbose=True)
+
+agent.invoke("How many days are left for Indian independence day?")
+# result = llm.invoke("what is gold price today in Indian money?")
+
+# print(result)  
